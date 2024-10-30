5f20ae8b40d323dba2ee19cf275c949e2b11f4e5
vishnu KP <vishnuto16@gmail.com>
2024-10-30 10:27:57 +0530

    new commit

--- server/src/middleware/multer.js
+++ server/src/middleware/multer.js
@@ -1,11 +1,10 @@
-// uploadConfig.js
 import multer from 'multer';
 import path from 'path';
 
-// Configure storage for Multer
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads'); // Set destination folder for uploads
+    cb(null, 'uploads'); 
   },
   filename: function (req, file, cb) {
     const fileType = file.mimetype.split('/')[1];
@@ -13,7 +12,6 @@ const storage = multer.diskStorage({
   }
 });
 
-// Initialize Multer with storage configuration
 const upload = multer({ storage });
 
 export default upload;
